301202ee1fae116c811cb94e7354782c0beb7d43
Nikita Lakhotia <nlakhotia91@gmail.com>
2015-09-12 17:34:52 +0530

    Final script

--- livestock_census_2012.py
+++ livestock_census_2012.py
@@ -2,29 +2,32 @@
 import urllib2
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from pandas import DataFrame, Series
 
 url = "http://farmer.gov.in/livestockcensus.aspx"
 r = requests.get(url)
 data = r.text
 soup = BeautifulSoup(data)
-## refers to the html part where data is stored
-tag_table = soup.find_all("table", style="text-align: left; width: 100%;" )
+tag_middlepn14 = soup.find(id="middlepnl4") 
+tag_table = tag_middlepn14.contents[3]
+tag_body = tag_table.contents[1]
 
-#tag_table[0]
-import pandas as pd
-from pandas import DataFrame, Series
-data_all=pd.DataFrame()
-for i in range(0,len(tag_table)):
-    data_text = tag_table[i].text #extract text from the tag 
-    data_replace = data_text.replace('\n', ',').replace(',,','').replace(' ','')
+output_path ='C:\\Users\\malaniaayushi\\Desktop\\'
+output_filename = 'final_data.csv'
+data_all=pd.DataFrame() #create an empty dataframe
+#range from second tag to the last tag which contains data
+for y in range(1,len(tag_body)-1): #ignore first tag as it contains column names
+    tag_data = tag_body.contents[y] 
+    data_text = tag_data.text # exhume data from the tag
+    data_replace = data_text.replace('\n', ',').replace(',,','').replace(' ','') #replace unwanted space, comma and line break
     data_string = data_replace.encode() #convert unicode into string
     data_list = data_string.split() # convert string into list
     df = pd.DataFrame(data_list) #convert into dataframe
     data_all =data_all.append(df) # append all iterations into final dataframe
-    #print data_all
-data_all.to_csv('C:\\Users\\malaniaayushi\\Desktop\\data.csv', index = False, sep='\t') #read 
-      
 
+data_all.to_csv(output_path + output_filename, header=False, index = False, sep='\t') # write data into csv file
+ 
     
     
 
